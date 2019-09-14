1dbaa9870a7cdd3699f7c0f77c0e86c8ba01ac1c
mrarslanark <m.arsanmushtaqahmed@gmail.com>
2019-09-14 21:38:08 +0500

    update: user interface
    adding necessary view componenets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let primaryQuote = `Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.`;
+    let quotations = `"${primaryQuote}"`;
+    let primaryAuthor = `Bernard M. Baruch`;
+    let author = `― ${primaryAuthor}`;
+    return (
+      <div>
+        <h1 className="HeadingStyle">Random Quote Machine</h1>
+        <div id="quote-box">
+          <p id="text">{quotations}</p>
+          <p id="author">{author}</p>
+          <div className="InteractionArea">
+            <button id="tweet-quote" type="submit">
+              <i class="fab fa-twitter"></i>
+            </button>
+            <button id="new-quote" type="submit">
+              New Quote
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+const data = [];
